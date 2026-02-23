b2f81318f80b23cbe0291b8309356536e573b9ac
Nicodemo Mazzaferro <nm3787@nyu.edu>
2026-02-23 14:41:08 -0500

    Add option to run OPES-f in EATR-OPES script

--- rates_eatr_opes.py
+++ rates_eatr_opes.py
@@ -29,8 +29,9 @@ barr.add_argument('--barriers', type=np.float64, help='the BARRIER parameter in 
 temperature.add_argument('--temp', type=np.float64, default=298, help='the temperature (in Kelvin) that the simulation was run at (make sure that ENERGYUNIT is correct)')
 temperature.add_argument('--kt', type=np.float64, default=None, help='the temperature (in kBT) that the simulation was run at')
 temperature.add_argument('--beta', type=np.float64, default=None, help='the inverse temperature 1/kBT that the simulation was run at')
-parser.add_argument('--tcol', type=int, default=0, help='the time column index in the COLVAR file.')
-parser.add_argument('--vcol', type=int, default=2, help='the bias column index in the COLVAR file.')
+parser.add_argument('--tcol', type=int, default=0, help='the time column index in the COLVAR file')
+parser.add_argument('--vcol', type=int, default=2, help='the bias column index in the COLVAR file')
+parser.add_argument('--acol', type=int, default=None, help='the acceleration factor column index in the COLVAR file (only useful if OPESF is set)')
 parser.add_argument('--timeunit', type=np.float64, default=1e-12, help='the conversion factor from the time unit used in PLUMED to seconds')
 parser.add_argument('--energyunit', type=np.float64, default=1, help='the conversion factor from the energy unit used in PLUMED to kJ/mol (only needed if temperature was given in Kelvin)')
 parser.add_argument('--gammamin', type=np.float64, default=0, help='the minimum value of gamma to be checked')
@@ -47,6 +48,7 @@ parser.add_argument('-q', '--quiet', action='store_true', help='do not print the
 parser.add_argument('--cdf', action='store_true', help='estimate the biased observed rates using CDF fitting (not recommended if you have arbitrarily right-censored data, such as simulations being killed before reaching max steps)')
 parser.add_argument('--timefirst', action='store_true', help='estimate ln<e^βγV> by averaging over time for each simulation, then over the simulations (default is over simulations first)')
 parser.add_argument('--nooffset', action='store_true', help='do not add the BARRIER parameter to the bias (OPES simulations in PLUMED offset the bias by -1*BARRIER, so do not use this for such simulations)')
+parser.add_argument('--opesf', action='store_true', help='also run the OPES flooding analysis on all of the data')
 
 args = parser.parse_args()
 
@@ -93,10 +95,14 @@ axis_first = 1 if args.timefirst else 0
 survival_f = lambda t, k: np.exp(-k*t)
 
 # Preload the data
-datas = [RM.get_data(colvars,args.tcol,args.vcol,time_scale_factor=args.timeunit) for colvars in args.input] # Yes I know data is the plural
+datas = [RM.get_data(colvars,args.tcol,args.vcol,acc_col=args.acol,time_scale_factor=args.timeunit) for colvars in args.input] # Yes I know data is the plural
 events = [RM.get_event(datas[i], maxlen=args.maxlen, maxtime=args.maxtime, num_events=num_eventss[i], log_files=log_filess[i], quiet=True) for i in range(len(datas))]
 
 def analyze(indicess, quiet=False):
+
+    logk0_opesf = None
+    opesf_times = []
+    opesf_event = []
 
     v_datas = {} # Yes I know data is the plural
     obs_rates = {}
@@ -110,8 +116,6 @@ def analyze(indicess, quiet=False):
         if not quiet:
             print(f'Simulation Set: BARRIER = {beta*barr} kBT')
             print(f'{event.sum()} out of {len(data)} simulations transitioned.')
-    
-        if not quiet:
             max_biases = [np.max(traj[:,1]+barr) for traj in data]
             print(f'avg. max. bias: {np.mean(max_biases)}')
     
@@ -123,6 +127,10 @@ def analyze(indicess, quiet=False):
         v_datas[barr] = v_data
     
         final_times = np.array([traj[-1,0] for traj in data])
+        if args.opesf:
+            rescaled_times = RM.iMetaD_rescaled_times(data, beta, bias_shift=barr_add)
+            opesf_times = opesf_times + list(rescaled_times)
+            opesf_event = opesf_event + list(event)
 
         # Fit the CDF to get the observed rate
         ecdfxs = np.sort(final_times)
@@ -144,6 +152,9 @@ def analyze(indicess, quiet=False):
             print(rf'ln<e^βV>: {np.log(avg)}')
             print('')
 
+    if args.opesf:
+        logk0_opesf = np.log(RM.iMetaD_invMRT_times(np.array(opesf_times), event=np.array(opesf_event)))
+
     def variance(gamma):
         logk0s = []
         for barr in barriers:
@@ -158,24 +169,26 @@ def analyze(indicess, quiet=False):
         logk0s.append(np.log(obs_rates[barr])-np.log(avg))
     logk0_best = np.mean(logk0s)
 
-    return logk0_best, gamma_best
+    return logk0_best, gamma_best, logk0_opesf
 
 
 
 if not args.bootstrap:
-    logk0_best, gamma_best = analyze([list(range(len(data))) for data in datas], quiet=args.quiet)
-    print(f'\nk0: {np.exp(logk0_best)} s^-1, logk0: {logk0_best} s^-1, τ0: {np.exp(-logk0_best)} s, gamma: {gamma_best}')
+    logk0_best, gamma_best, logk0_opes = analyze([list(range(len(data))) for data in datas], quiet=args.quiet)
+    print(f"\nk0: {np.exp(logk0_best)} s^-1, logk0: {logk0_best} s^-1, τ0: {np.exp(-logk0_best)} s, gamma: {gamma_best}{', OPES logk0: '+str(logk0_opes)+' s^-1' if args.opesf else ''}")
 else:
     sample_logk0 = []
     sample_gamma = []
+    sample_opesf = []
     for i in range(args.numboots):
         indicess = [random.choices(list(range(len(data))), k=len(data)) for data in datas]
-        logk0, gamma = analyze(indicess, quiet=True)
+        logk0, gamma, logk0_opesf = analyze(indicess, quiet=True)
         sample_logk0.append(logk0)
         sample_gamma.append(gamma)
+        sample_opesf.append(logk0_opesf)
         if not args.quiet:
             print(i)
-    print(f'logk0: {np.mean(sample_logk0)} +/- σ {np.std(sample_logk0)} s^-1, τ0: {np.exp(-np.mean(sample_logk0))} s, gamma: {np.mean(sample_gamma)} +/- σ {np.std(sample_gamma)}')
+    print(f"logk0: {np.mean(sample_logk0)} +/- σ {np.std(sample_logk0)} s^-1, τ0: {np.exp(-np.mean(sample_logk0))} s, gamma: {np.mean(sample_gamma)} +/- σ {np.std(sample_gamma)}{', OPES logk0: '+str(np.mean(sample_opesf))+' +/- σ '+str(np.std(sample_opesf))+' s^-1' if sample_opesf[0] is not None else ''}")
     
     """
     if boots_avail:
